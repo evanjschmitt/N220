00c3cf9d256a11844caad9539a7c6c5d3b31c1e0
evanjschmitt <evanjschmitt@gmail.com>
2023-11-14 19:02:24 -0500

    FINISHED W 8

--- homeworkEight/advList.js
+++ homeworkEight/advList.js
@@ -1,18 +1,49 @@
 //Add Trash Can
-var i = 0;
+// var i = 0;
 var listItems = document.querySelectorAll("#codeBox li");
 var totalItems = listItems.length;
-var image = document.createElement("img");
-console.log(totalItems)
-for (i; totalItems > i; i++) 
-{console.log("hello");
-  
+for (let i = 0; totalItems > i; i++) {
+  console.log(i);
+  var trashIcon = document.createElement("img");
 
-  image.setAttribute(
+  trashIcon.setAttribute(
     "src",
     "https://s3-us-west-2.amazonaws.com/s.cdpn.io/27019/trashcanIcon.png"
   );
-  image.setAttribute("class", "listIcon");
-  listItems[i].appendChild(image);
-  console.log(image);
+  trashIcon.setAttribute("class", "listIcon");
+  listItems[i].appendChild(trashIcon);
 }
+
+//Trash Items
+var myList = document.querySelector("#codeBox ul");
+
+function changeProp(e) {
+  var target = getTarget(e);
+  console.log(target);
+  let tParent = target.parentNode;
+  console.log(tParent);
+  if (tParent.tagName == "LI") {
+    tParent.parentNode.removeChild(tParent);
+  }
+  if (target.className == "selected") {
+    console.log("hello");
+    target.classList.remove("selected");
+  } else {
+    target.setAttribute("class", "selected");
+    console.log("hello2");
+  }
+}
+
+function getTarget(e) {
+  if (!e) {
+    e = window.event;
+  } else {
+    return e.target || e.srcElement;
+  }
+
+  if (!target) {
+    console.error("Target is undefined.");
+    return;
+  }
+}
+myList.addEventListener("click", changeProp, false);
