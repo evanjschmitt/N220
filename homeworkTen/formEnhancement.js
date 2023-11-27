3d890ef9b5a214dcf90bee2f84d6da537d17385c
evanjschmitt <evanjschmitt@gmail.com>
2023-11-27 15:54:05 -0500

    Homework Ten Start

---
+++ homeworkTen/formEnhancement.js
@@ -0,0 +1,5 @@
+var passForm = document.forms[0];
+var passBtn = passForm.getElementsByTagName('img')[0];
+
+passBtn.addEventListener('click', showPassword())
+passBtn.addEventListener('click', showPassword())
