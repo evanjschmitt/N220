943adea621f68e09e50915b08f3161951d5759e9
evanjschmitt <evanjschmitt@gmail.com>
2023-11-27 22:58:27 -0500

    HW 10 Stuck

--- homeworkTen/formEnhancement.js
+++ homeworkTen/formEnhancement.js
@@ -1,5 +1,28 @@
 var passForm = document.forms[0];
-var passBtn = passForm.getElementsByTagName('img')[0];
+var passBtn = passForm.getElementsByTagName("img")[0];
 
-passBtn.addEventListener('click', showPassword())
-passBtn.addEventListener('click', showPassword())
+function showPassword() {
+  var imgSrc = passBtn.getAttribute("src");
+  var hidePassImg = imgSrc.indexOf("hide");
+  console.log(imgSrc);
+  console.log(hidePassImg);
+  if (hidePassImg > 0) {
+    document
+      .getElementById("pass")
+      .setAttribute(
+        "src",
+        "https://s3-us-west-2.amazonaws.com/s.cdpn.io/27019/showPassIcon.png"
+      );
+    document.getElementById("pass").setAttribute("type", "text");
+  } else {
+    document
+      .getElementById("pass")
+      .setAttribute(
+        "src",
+        "https://s3-us-west-2.amazonaws.com/s.cdpn.io/27019/hidePassIcon.png"
+      );
+    document.getElementById("pass").setAttribute("type", "password");
+  }
+}
+passBtn.addEventListener("click", showPassword);
+
