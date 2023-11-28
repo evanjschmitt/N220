5209af7a1350561454826548dbddcd819a207c9f
evanjschmitt <evanjschmitt@gmail.com>
2023-11-27 23:44:01 -0500

    FIXED IT

--- homeworkTen/formEnhancement.js
+++ homeworkTen/formEnhancement.js
@@ -7,22 +7,17 @@ function showPassword() {
   console.log(imgSrc);
   console.log(hidePassImg);
   if (hidePassImg > 0) {
-    document
-      .getElementById("pass")
-      .setAttribute(
-        "src",
-        "https://s3-us-west-2.amazonaws.com/s.cdpn.io/27019/showPassIcon.png"
-      );
+    passBtn.setAttribute(
+      "src",
+      "https://s3-us-west-2.amazonaws.com/s.cdpn.io/27019/showPassIcon.png"
+    );
     document.getElementById("pass").setAttribute("type", "text");
   } else {
-    document
-      .getElementById("pass")
-      .setAttribute(
-        "src",
-        "https://s3-us-west-2.amazonaws.com/s.cdpn.io/27019/hidePassIcon.png"
-      );
+    passBtn.setAttribute(
+      "src",
+      "https://s3-us-west-2.amazonaws.com/s.cdpn.io/27019/hidePassIcon.png"
+    );
     document.getElementById("pass").setAttribute("type", "password");
   }
 }
 passBtn.addEventListener("click", showPassword);
-
